2cb4004b5a572ff66d42e431a1e1bb2e98e1f1a0
Md Ruhul Amin <ra.01403288711@gmail.com>
2021-10-25 13:24:11 +0600

    Dashboard logic is updated

--- resources/views/Dashboard/TodayAndTomorrow.blade.php
+++ resources/views/Dashboard/TodayAndTomorrow.blade.php
@@ -14,21 +14,18 @@
       h2{
           font-size:40px;
       }
-     
-     
   </style>
 </head>
 <body>
-<div class="card">
+<div class="card m-3">
     <div class="row">
         <div class="col-sm-6 m-2">
             <div class="card">
                 <h1 align="center"> Today </h1>
                 <h1 align="center"> Location - NOC </h1>
-
                 <div class="row">
                     <div class="col-sm-2">
-                        @if (0<count($combineAllInformation_today_NOC))
+                        @if (!empty($combineAllInformation_today_NOC[0]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_NOC[0]['employeeDetails']['image'])}}"/>
@@ -48,7 +45,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (1<count($combineAllInformation_today_NOC))
+                        @if (!empty($combineAllInformation_today_NOC[1]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_NOC[1]['employeeDetails']['image'])}}"/>
@@ -68,7 +65,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (1<count($combineAllInformation_today_NOC))
+                        @if (!empty($combineAllInformation_today_NOC[2]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_NOC[2]['employeeDetails']['image'])}}"/>
@@ -90,7 +87,7 @@
                 </div>
                 <div class="row">
                     <div class="col-sm-2">
-                        @if (3<count($combineAllInformation_today_NOC))
+                        @if ( !empty($combineAllInformation_today_NOC[3]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_NOC[3]['employeeDetails']['image'])}}"/>
@@ -110,7 +107,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (4<count($combineAllInformation_today_NOC))
+                        @if (!empty($combineAllInformation_today_NOC[4]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_NOC[4]['employeeDetails']['image'])}}"/>
@@ -130,7 +127,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (5<count($combineAllInformation_today_NOC))
+                        @if (!empty($combineAllInformation_today_NOC[5]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_NOC[5]['employeeDetails']['image'])}}"/>
@@ -155,7 +152,7 @@
                 <h1 align="center">Location - Factory </h1>
                 <div class="row">
                     <div class="col-sm-2">
-                        @if (0<count($combineAllInformation_today_Factory))
+                        @if (!empty($combineAllInformation_today_Factory[0]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_Factory[0]['employeeDetails']['image'])}}"/>
@@ -175,7 +172,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (1<count($combineAllInformation_today_Factory))
+                        @if (!empty($combineAllInformation_today_Factory[1]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_Factory[1]['employeeDetails']['image'])}}"/>
@@ -186,6 +183,7 @@
                                     <tr>
                                         <td><h2>OUT</h2></td><td><h2>{{$combineAllInformation_today_Factory[1]['taskList']['endTime']}}</h2></td>
                                     </tr>
+
                                     <tr>
                                         <td><h3>Total </h3></td><td><h3>{{$combineAllInformation_today_Factory[1]['taskList']['totalTime']}}</h3></td>
                                     </tr>
@@ -195,7 +193,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (1<count($combineAllInformation_today_Factory))
+                        @if (!empty($combineAllInformation_today_Factory[2]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_Factory[2]['employeeDetails']['image'])}}"/>
@@ -217,7 +215,7 @@
                 </div>
                 <div class="row">
                     <div class="col-sm-2">
-                        @if (3<count($combineAllInformation_today_Factory))
+                        @if (!empty($combineAllInformation_today_Factory[3]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_Factory[3]['employeeDetails']['image'])}}"/>
@@ -237,7 +235,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (4<count($combineAllInformation_today_Factory))
+                        @if (!empty($combineAllInformation_today_Factory[4]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_Factory[4]['employeeDetails']['image'])}}"/>
@@ -257,7 +255,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (5<count($combineAllInformation_today_Factory))
+                        @if (!empty($combineAllInformation_today_Factory[5]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_today_Factory[5]['employeeDetails']['image'])}}"/>
@@ -289,7 +287,7 @@
                 <h1 align="center"> Location - NOC </h1>
                 <div class="row">
                     <div class="col-sm-2">
-                        @if (0<count($combineAllInformation_tomorrow_NOC))
+                        @if (!empty($combineAllInformation_tomorrow_NOC[0]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_NOC[0]['employeeDetails']['image'])}}"/>
@@ -309,7 +307,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (1<count($combineAllInformation_tomorrow_NOC))
+                        @if (!empty($combineAllInformation_tomorrow_NOC[1]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_NOC[1]['employeeDetails']['image'])}}"/>
@@ -328,8 +326,8 @@
                         </div>
                          @endif
                     </div>
-                    {{-- <div class="col-sm-2">
-                        @if (1<count($combineAllInformation_tomorrow_NOC))
+                    <div class="col-sm-2">
+                        @if (!empty($combineAllInformation_tomorrow_NOC[2]))
                         <div class="card">
                             <div class="card-body">
                                 @if (1<count($combineAllInformation_tomorrow_NOC))
@@ -350,11 +348,11 @@
                             </div>
                         </div>
                          @endif
-                    </div> --}}
+                    </div>
                 </div>
                 <div class="row">
                     <div class="col-sm-2">
-                        @if (3<count($combineAllInformation_tomorrow_NOC))
+                        @if (!empty($combineAllInformation_tomorrow_NOC[3]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_NOC[3]['employeeDetails']['image'])}}"/>
@@ -374,7 +372,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (4<count($combineAllInformation_tomorrow_NOC))
+                        @if (!empty($combineAllInformation_tomorrow_NOC[4]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_NOC[4]['employeeDetails']['image'])}}"/>
@@ -394,7 +392,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (5<count($combineAllInformation_tomorrow_NOC))
+                        @if (!empty($combineAllInformation_tomorrow_NOC[5]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_NOC[5]['employeeDetails']['image'])}}"/>
@@ -419,7 +417,7 @@
                 <h1 align="center"> Location - Factory  </h1>
                 <div class="row">
                     <div class="col-sm-2">
-                        @if (0<count($combineAllInformation_tomorrow_Factory))
+                        @if (!empty($combineAllInformation_tomorrow_Factory[0]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_Factory[0]['employeeDetails']['image'])}}"/>
@@ -439,7 +437,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (1<count($combineAllInformation_today_Factory))
+                        @if (!empty($combineAllInformation_tomorrow_Factory[1]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_Factory[1]['employeeDetails']['image'])}}"/>
@@ -459,7 +457,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (1<count($combineAllInformation_tomorrow_Factory))
+                        @if (!empty($combineAllInformation_tomorrow_Factory[2]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_Factory[2]['employeeDetails']['image'])}}"/>
@@ -481,7 +479,7 @@
                 </div>
                 <div class="row">
                     <div class="col-sm-2">
-                        @if (3<count($combineAllInformation_tomorrow_Factory))
+                        @if (!empty($combineAllInformation_tomorrow_Factory[3]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_Factory[3]['employeeDetails']['image'])}}"/>
@@ -501,7 +499,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (4<count($combineAllInformation_tomorrow_Factory))
+                        @if (!empty($combineAllInformation_tomorrow_Factory[4]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_Factory[4]['employeeDetails']['image'])}}"/>
@@ -521,7 +519,7 @@
                          @endif
                     </div>
                     <div class="col-sm-2">
-                        @if (5<count($combineAllInformation_tomorrow_Factory))
+                        @if (!empty($combineAllInformation_tomorrow_Factory[5]))
                         <div class="card">
                             <div class="card-body">
                                 <img width="300px" height="300px" src="{{url('storage/'.$combineAllInformation_tomorrow_Factory[5]['employeeDetails']['image'])}}"/>
